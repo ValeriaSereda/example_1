5c9b29464528b76c2e66bc0bf19bef46a6028214
ValeriaSereda <seredylka.lera@gmail.com>
2023-04-16 17:21:33 +0200

    Dodany trojkat

---
+++ ex_2.py
@@ -0,0 +1,14 @@
+# trojkat
+# deklaracja zmiennych 
+a = 10
+b = 12
+c = 8
+h = 6
+
+obwod = a + b + c
+pole = 1/2 * a * h 
+
+print("Obwod trojkata wynosi " + str(obwod) + ", zas pole wynosi " + str(pole) + ".")
+
+
+
