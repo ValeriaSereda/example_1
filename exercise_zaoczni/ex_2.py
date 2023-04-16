0378ad6efe2e2ec077c9bd7236d63f3a8c1bb191
ValeriaSereda <seredylka.lera@gmail.com>
2023-04-16 17:32:21 +0200

    Dodano romb

--- exercise_zaoczni/ex_2.py
+++ exercise_zaoczni/ex_2.py
@@ -10,5 +10,13 @@ pole = 1/2 * a * h
 
 print("Obwod trojkata wynosi " + str(obwod) + ", zas pole wynosi " + str(pole) + ".")
 
+# romb
+# TODO
 
+a = 5
+h = 4
 
+pole = a * h 
+obwod = 4 * a 
+
+print("Pole rombu wynosi " + str(pole) + ", zas obwod wynosi " + str(obwod) + ".")
